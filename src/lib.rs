e41342813096aa84200f065ad533f120b2f00daf
CDFMLR <cdfmlr@163.com>
2024-01-09 17:06:43 +0800

    eprintln execed commands

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,8 @@ where
     let base64_json_args = general_purpose::STANDARD_NO_PAD.encode(json_encoded_args.as_bytes());
     debug!("&base64_json_args = {:?}", &base64_json_args);
 
+    eprintln!("# z2fish > /bin/zsh -lic '{} {} {}'", cmd, SHELL_PREFIX, &base64_json_args);
+
     Command::new("/bin/zsh")
         .arg("-lic")
         .arg(format!("{} {} {}", cmd, SHELL_PREFIX, base64_json_args))
@@ -53,6 +55,8 @@ pub fn run_shell(base64_json_args: &str) {
 
     let args: Vec<String> = serde_json::from_str(json_encoded_args).unwrap();
     debug!("&args = {:?}", &args);
+
+    eprintln!("# z2fish > fish {:?}", &args);
 
     Command::new("fish").args(args).exec();
 }
